6b761e9868e6e1ec87e669a9dac49064381de36c
Cory Latschkowski <koree@redhat.com>
2023-05-26 16:15:03 -0500

    update: model training py

--- model-training/model_training.py
+++ model-training/model_training.py
@@ -1,5 +1,15 @@
+# %%
+# install dependencies
+%pip install -q -U pip
+%pip install -q -r requirements.txt
+
+# %%
+# setup parameters
+scratch = "../scratch/"
+
+# %%
 import numpy as np
-import os, random, pathlib, warnings, itertools, math
+import os, random, pathlib, warnings, itertools, math, json
 import tensorflow as tf
 import tensorflow.keras.backend as K
 from sklearn.metrics import confusion_matrix
@@ -8,6 +18,12 @@ import pandas as pd
 import seaborn as sns
 from PIL import Image
 from PIL import ImageEnhance
+from shutil import rmtree
+
+import boto3
+import botocore
+from botocore import UNSIGNED
+from botocore.client import Config
 
 from tensorflow.keras import models
 from tensorflow.keras.models import Model
@@ -22,110 +38,18 @@ from tensorflow.keras.layers import (
     Dropout,
 )
 
+# %%
 K.clear_session()
 
-dataset = "./Vegetable Images"
+# %%
+# setup paths for data
+dataset = scratch + "Vegetable Images"
 
 train_folder = os.path.join(dataset, "train")
 test_folder = os.path.join(dataset, "validation")
 validation_folder = os.path.join(dataset, "test")
 
-
-def count_files(rootdir):
-    """counts the number of files in each subfolder in a directory"""
-    for path in pathlib.Path(rootdir).iterdir():
-        if path.is_dir():
-            print(
-                "There are "
-                + str(
-                    len(
-                        [
-                            name
-                            for name in os.listdir(path)
-                            if os.path.isfile(os.path.join(path, name))
-                        ]
-                    )
-                )
-                + " files in "
-                + str(path.name)
-            )
-
-
-count_files(os.path.join(test_folder))
-
-
-def Preprocess(number_of_images):
-    j = 1
-    for i in range(number_of_images):
-        folder = os.path.join(test_folder, image_folder)
-        a = random.choice(os.listdir(folder))
-
-        image = Image.open(os.path.join(folder, a))
-        image_duplicate = image.copy()
-        plt.figure(figsize=(10, 10))
-
-        plt.subplot(number_of_images, 2, j)
-        plt.title(label="Orignal", size=17, pad="7.0", loc="center", fontstyle="italic")
-        plt.imshow(image)
-        j += 1
-
-        image1 = ImageEnhance.Color(image_duplicate).enhance(1.35)
-        image1 = ImageEnhance.Contrast(image1).enhance(1.45)
-        image1 = ImageEnhance.Sharpness(image1).enhance(2.5)
-
-        plt.subplot(number_of_images, 2, j)
-        plt.title(
-            label="Processed", size=17, pad="7.0", loc="center", fontstyle="italic"
-        )
-        plt.imshow(image1)
-        j += 1
-
-
-image_folder = "Cucumber"  # Tclearhe vegetable you want to display
-number_of_images = 2
-Preprocess(number_of_images)
-
-select_vegetable = "Capsicum"
-rows, columns = 1, 5
-
-display_folder = os.path.join(train_folder, select_vegetable)
-total_images = rows * columns
-fig = plt.figure(1, figsize=(20, 10))
-
-for i, j in enumerate(os.listdir(display_folder)):
-    print(j)
-    if j.startswith("."):
-        continue
-    img = plt.imread(os.path.join(train_folder, select_vegetable, j))
-    fig = plt.subplot(rows, columns, i + 1)
-    fig.set_title(select_vegetable, pad=11, size=20)
-    plt.imshow(img)
-
-    if i == total_images - 1:
-        break
-
-images = []
-
-for food_folder in sorted(os.listdir(train_folder)):
-    if food_folder.startswith("."):
-        continue
-    food_items = os.listdir(train_folder + "/" + food_folder)
-    food_selected = np.random.choice(food_items)
-    images.append(os.path.join(train_folder, food_folder, food_selected))
-
-fig = plt.figure(1, figsize=(15, 10))
-
-for subplot, image_ in enumerate(images):
-    category = image_.split("/")[-2]
-    imgs = plt.imread(image_)
-    a, b, c = imgs.shape
-    fig = plt.subplot(3, 5, subplot + 1)
-    fig.set_title(category, pad=10, size=18)
-    plt.imshow(imgs)
-
-plt.tight_layout()
-
-
+# %%
 IMAGE_SIZE = [224, 224]
 
 inception = InceptionV3(
@@ -165,12 +89,57 @@ test_set = test_datagen.flow_from_directory(
 class_map = training_set.class_indices
 print(class_map)
 
-r = model.fit(
-    training_set,
-    validation_data=test_set,
-    epochs=5,
-    steps_per_epoch=len(training_set),
-    validation_steps=len(test_set),
-)
+# %%
+model_metadata = "_inceptionV3_epoch5"
+path_to_model = scratch + "model" + model_metadata + ".h5"
 
-model.save("model/model_inceptionV3_epoch5.h5")
+# %%
+if not os.path.exists(path_to_model):
+    r = model.fit(
+        training_set,
+        validation_data=test_set,
+        epochs=5,
+        steps_per_epoch=len(training_set),
+        validation_steps=len(test_set),
+    )
+    
+    model.save(path_to_model)
+
+else:
+    print("Model: already exists")
+
+
+# %%
+K.clear_session()
+
+print("Model: Loading...")
+model = load_model(path_to_model)
+print("Model: Loaded")
+
+# %%
+category = {v: k for k, v in class_map.items()}
+print(json.dumps(category, indent=4))
+
+
+def predict_image(filename, model):
+    img_ = image.load_img(filename, target_size=(224, 224))
+    img_array = image.img_to_array(img_)
+    img_processed = np.expand_dims(img_array, axis=0)
+    img_processed /= 255.0
+
+    prediction = model.predict(img_processed)
+    index = np.argmax(prediction)
+
+    plt.title("Prediction - {}".format(category[index]))
+    plt.imshow(img_array)
+
+# %%
+predict_image(os.path.join(validation_folder, "Cauliflower/1064.jpg"), model)
+
+# %%
+predict_image(os.path.join(validation_folder, "Bitter_Gourd/1202.jpg"), model)
+
+# %%
+predict_image(os.path.join(validation_folder, "Papaya/1266.jpg"), model)
+
+
